eee9513e1951609fe9438413daa5923a66d9db3f
JohannSako <johann.sako@epitech.eu>
2024-10-11 20:36:45 +0200

    [ADD] Category components

--- photo-contest/tailwind.config.js
+++ photo-contest/tailwind.config.js
@@ -12,6 +12,40 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        'check-empty': '#F6F6F6',
+        'green-primary': '#5DB075',
+        'green-secondary': '#4B9460',
+        'white-6': "#F6F6F6",
+        'white-8': "#F8F8F8",
+        'gray-8': '#E8E8E8',
+        'background': '#F9F9F9',
+      },
+      borderColor: {
+        'gray-8': '#E8E8E8',
+        'green-secondary': '#4B9460',
+      },
+      colors: {
+        'green-primary': '#5DB075',
+        'green-secondary': '#4B9460',
+        'white-6': "#F6F6F6",
+        'white-8': "#F8F8F8",
+        'gray-01': '#F6F6F6',
+        'gray-02': '#E8E8E8',
+        'gray-03': '#BDBDBD',
+        'primary': '#5DB075',
+        'disabled': '#979797',
+      },
+      placeholderColor: {
+        'primary': "#BDBDBD"
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
+      boxShadow: {
+        'default': '0px 1px 4px 0px rgba(0, 0, 0, 0.25)',
+        'inset': '0px 1px 4px 0px rgba(0, 0, 0, 0.25) inset',
+      },
     },
   },
   plugins: [],
