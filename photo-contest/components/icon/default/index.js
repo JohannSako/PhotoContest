6d12a120267999666da1806e9220f246d2cbf05f
JohannSako <johann.sako@epitech.eu>
2024-10-14 09:20:47 +0200

    [ADD] Icon component

---
+++ photo-contest/components/icon/default/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@fontsource/quicksand';
+import '@fontsource/quattrocento-sans';
+
+export default function Icon({}) {
+  return (
+    <div className="flex flex-col items-center">
+      <p className="text-white text-center font-quicksand text-[36px] font-normal leading-normal">
+        PhotoContest
+      </p>
+    </div>
+  );
+};
