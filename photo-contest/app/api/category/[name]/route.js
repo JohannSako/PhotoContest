c724faec9ef4c6075ac107f25f041b41cea31122
JohannSako <johann.sako@epitech.eu>
2024-11-01 14:50:42 +0100

    [ADD] Api main routes

---
+++ photo-contest/app/api/category/[name]/route.js
@@ -0,0 +1,61 @@
+import clientPromise from '@/lib/mongodb';
+import { ObjectId } from 'mongodb';
+
+export async function GET(request, { params }) {
+  try {
+    const client = await clientPromise;
+    const db = client.db('admin');
+    const categoryCollection = db.collection('category');
+    const themeCollection = db.collection('theme');
+
+    const { name } = params;
+
+    const category = await categoryCollection.findOne({ title: name });
+
+    if (!category) {
+      return new Response(JSON.stringify({ error: 'Category not found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const themeObjectIds = [];
+    category.themes.forEach(id => {
+      try {
+        themeObjectIds.push(new ObjectId(id['$id']));
+      } catch (e) {
+        console.error('Invalid ObjectId:', id, e);
+      }
+    });
+
+    if (themeObjectIds.length === 0) {
+      return new Response(JSON.stringify({ error: 'No valid theme IDs found' }), {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const themes = await themeCollection.find({ _id: { $in: themeObjectIds } }).toArray();
+    console.log('Fetched themes:', themes);
+
+    return new Response(JSON.stringify({ ...category, themes }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching category:', error.message);
+
+    return new Response(JSON.stringify({ error: `Error fetching category: ${error.message}` }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
