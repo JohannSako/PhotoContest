7caa779dc0c9a016c54043ff1814244f49b75317
JohannSako <johann.sako@epitech.eu>
2024-11-08 15:34:31 +0100

    [ADD] home screen with delete game

--- photo-contest/app/home/calendar/page.js
+++ photo-contest/app/home/calendar/page.js
@@ -1,1 +1,9 @@
-export default function HomeCalendar() { return <div></div> }
+import Text from "@/components/text";
+
+export default function HomeCalendar() {
+    return (
+        <div className="flex h-[100vh] bg-primary items-center justify-center">
+            <Text color="white" weight="700" size="20px">Working on it hehe :')</Text>
+        </div>
+    )
+}
