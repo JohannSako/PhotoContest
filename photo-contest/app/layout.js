4caa917b381ed02a690340f1ee83ca2e5ea635a1
JohannSako <johann.sako@epitech.eu>
2024-12-12 22:03:53 +0100

    [ADD] PWA for WebApp

--- photo-contest/app/layout.js
+++ photo-contest/app/layout.js
@@ -2,8 +2,8 @@ import './globals.css';
 import MainLayout from '@/components/layout/main';
 
 export const metadata = {
-  title: 'Photo Contest',
-  description: 'A photo contest application',
+  title: 'PhotoContest',
+  description: 'Snap, Share, Succeed',
 };
 
 export default function RootLayout({ children }) {
@@ -12,6 +12,8 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <title>{metadata.title}</title>
       </head>
       <body>
